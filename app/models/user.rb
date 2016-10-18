af2e9a33b5d812680d7747896fef70c3eb325362
Pete <pete@coderfactory.com>
2016-10-18 21:15:25 +1100

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  after_create :assign_role
+
+  def assign_role
+    if user_type == 'owner'
+      add_role :owner
+    end
+  end
+end
